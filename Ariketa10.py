e6f8614657d155d849e0db9c3000bc98e6f8c779
ioritzlope45@gmail.com <ioritzlope45@gmail.com>
2024-11-18 12:53:07 +0100

    Ariketa10 eginda

---
+++ Ariketa10.py
@@ -0,0 +1,6 @@
+dis_zentimetro = input ("Sartu distantzia zentimetroetan: ")
+
+km = float(dis_zentimetro) /1000
+m = float(dis_zentimetro) / 100
+
+print(str(dis_zentimetro) + " zentimetro = " + str(km) + " kilometro eta  " + str(m) + " metro")
