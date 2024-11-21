6c990cba22ed87c9c5cef25ddf34dc87052449fd
ioritzlope45@gmail.com <ioritzlope45@gmail.com>
2024-11-21 12:54:57 +0100

    IterazioAriketa27 eginda

---
+++ IterazioAriketak27.py
@@ -0,0 +1,11 @@
+import random
+
+while True:
+    input("Sakatu enter ausazko zenbakiak sortzeko: ")
+    zenbakia = random.randint(1, 6)
+    print("Ausazko Zenbakia: " + str(zenbakia))
+    
+    erantzuna = input("Beste ausazko zenbakik bat lortu nahi duzu? (bai/ez): ").lower()
+    if erantzuna != "bai":
+        print("Agur!")
+        break
